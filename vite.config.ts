38a4ca1aa7c2a69d126c9c7051086e4e6481a59a
Hema <hemakesh.ande@gmail.com>
2025-10-29 21:31:56 +0100

    chore: remove duplicate base config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   base: "/Thai-Nippon-Sushi-Bar/",
-  base: "/Thai-Nippon-Sushi-Bar/",
   server: {
     host: "::",
     port: 8080,
